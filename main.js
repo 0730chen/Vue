ba5f1deff11f63497f4102bd7dfe4ffec8a82f4b
0730chen <1252331215@qq.com>
2019-09-05 18:26:06 +0800

    -m学习如何实现bind call apply

--- main.js
+++ main.js
@@ -15,32 +15,91 @@
 //     children.uber = parsent.prototypes
 // }
 //使用node连接mongodb数据库，操作数据
-
-const MongoClient = require('mongodb').MongoClient
-const url = "mongodb://localhost:27017"
-const dbName = 'db'
-
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-    if (err) {
-        console.log(err)
+Function.prototype.myCall = function(context = window, ...args) {
+    if (this === Function.prototype) {
+        return undefined
     }
-    console.log("连接成功")
-    const db = client.db(dbName)
-    const col = db.collection('db')
-    col.insert({ "1": "这是第一个" })
-    data = { name1: '大强苏', age: "12" }
-    col.insert(data, function(err, callback) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("成功")
+    context = context || window
+    const fn = Symbol()
+    context[fn] = this
+    const result = context[fn](...args)
+    delete context[fn]
+    return result
+}
+
+var a = {
+    name: 'haha',
+    age: 18,
+}
+var name = 'lili'
+
+function get() {
+    console.log(this.name)
+}
+get.myCall()
+
+//实现apply 
+Function.prototype.myApply = function(context = window, args) {
+        if (this === Function.prototype) {
+            return undefined
         }
-
-    })
-    col.find().toArray((err, res) => {
-        console.log(res)
-
-    })
-})
-var m = require('./es6')
-console.log(m)
+        const fn = Symbol()
+        context[fn] = this
+        if (Array.isArray(arg)) {
+            let result = context[fn](...arg)
+        } else {
+            let result = context[fn]()
+        }
+        delete context[fn]
+        return fn
+    }
+    //bing实现
+Function.prototype.Mybind = function(context = window, ...arg1) {
+        if (this === Function.prototype) {
+            return undefined
+        }
+        const _this = this
+        return function F(...arg2) {
+            if (this instanceceof F) {
+                return new _this(...arg1, ...arg2)
+            }
+            return _this.apply(context, arg1.concat(arg2))
+        }
+    }
+    //防抖，函数在事件触发后的一段时间才执行
+function debounce(event, time, flag) {
+    let timer = null
+    return function(...args) {
+        cleatTimeout(timer)
+            //flag添加判断，如果开始时，null为空，flag为true则触发一次，之后的定时触发
+        if (falg && !timer) {
+            evetn.apply(this, args)
+        }
+        timer = setTimeout(() => {
+            evetn.apply(this, args)
+        }, time);
+    }
+}
+//节流，在规定时间内只触发一次
+//第一次不触发,最后一次触发
+function throttle(event, time) {
+    let pre = 0
+    return function(...args) {
+        if (Date.now() - pre > time) {
+            pre = Data.now()
+            event.apply(this, args)
+        }
+    }
+}
+//第一次触发，最后一次不触发
+function throttle(event, time) {
+    let time = null
+    return function(...args) {
+        if (!time) {
+            timer = setTimeout(() => {
+                timer = null
+                evetn.apply(this, args)
+            }, time)
+        }
+    }
+}
